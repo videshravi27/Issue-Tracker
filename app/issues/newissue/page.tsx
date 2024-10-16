40317b5535938b4bda05b3b2fb3ba8942cbe5ab2
Videsh Ravi <videshravi27@gmail.com>
2024-10-16 22:43:06 +0530

    Error handling in form submission

--- app/issues/newissue/page.tsx
+++ app/issues/newissue/page.tsx
@@ -1,23 +1,26 @@
 'use client';
 
-import { Button, Callout, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField, Text } from '@radix-ui/themes'
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form"
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { valid } from '@/app/valid';
+import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof valid>;
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
 
 const NewIssuePage = () => {
     const router = useRouter()
-    const { register, control, handleSubmit } = useForm<IssueForm>()
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(valid)
+    })
     const [error, setError] = useState('')
 
     return (
@@ -40,6 +43,9 @@ const NewIssuePage = () => {
                 })}>
                 <TextField.Root placeholder="Title" {...register('title')}>
                 </TextField.Root>
+                <ErrorMessage>
+                    {errors.title?.message}
+                </ErrorMessage>
                 <Controller
                     name="description"
                     control={control}
@@ -51,6 +57,9 @@ const NewIssuePage = () => {
                         ref={ref}
                     />}
                 />
+                <ErrorMessage>
+                    {errors.description?.message}
+                </ErrorMessage>
                 <Button >Submit</Button>
             </form >
         </div>
