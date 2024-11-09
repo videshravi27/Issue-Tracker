f49efccba7c5c55c1b7a5f4022a7857bb9d13828
Videsh Ravi <videshravi27@gmail.com>
2024-11-10 00:10:42 +0530

    update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,7 +36,10 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     const onSubmit = handleSubmit(async (data) => {
         try {
             setSubmitting(true);
-            await axios.post('/api/issues', data)
+            if (issue)
+                await axios.patch('/api/issues/' + issue.id, data);
+            else
+                await axios.post('/api/issues', data)
             router.push('/issues');
         } catch (error) {
             setSubmitting(false);
@@ -75,7 +78,10 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
                 <ErrorMessage>
                     {errors.description?.message}
                 </ErrorMessage>
-                <Button disabled={isSubmitting}>Submit {isSubmitting && <Spinner />}</Button>
+                <Button disabled={isSubmitting}>
+                    {issue ? 'Update Issue' : 'Submit'}{' '}
+                    {isSubmitting && <Spinner />}
+                </Button>
             </form >
         </div>
     )
