dddcdff810373c44fca5419143b2c49389de80c6
Videsh Ravi <videshravi27@gmail.com>
2024-11-10 16:17:14 +0530

    handled errors while deleting

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,36 +3,58 @@
 import { Button, AlertDialog, Flex } from '@radix-ui/themes'
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, serError] = useState(false)
+
+  const onDelete = async () => {
+    try {
+      await axios.delete('/api/issues/' + issueId);
+      router.push('/issues');
+      router.refresh();
+    } catch (error) {
+      serError(true);
+    }
+  }
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color='red'>Delete</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue?
-        </AlertDialog.Description>
-        <Flex mt="4" gap="2">
-          <AlertDialog.Cancel>
-            <Button variant='soft' color="gray">Cancel</Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button color='red' onClick={async () => {
-              await axios.delete('/api/issues/' + issueId);
-              router.push('/issues');
-              router.refresh();
-            }}>
-              Delete
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color='red'>Delete</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue?
+          </AlertDialog.Description>
+          <Flex mt="4" gap="2">
+            <AlertDialog.Cancel>
+              <Button variant='solid' color="gray">Cancel</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color='red' onClick={onDelete}>
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            There was an error deleting the issue. Please try again later.
+          </AlertDialog.Description>
+          <Button mt="2" variant='solid' color="gray" onClick={() => serError(false)}>
+            Close
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   )
 }
 
