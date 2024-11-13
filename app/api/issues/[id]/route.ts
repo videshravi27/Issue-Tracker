64b44775ef32a219c69d6dbc4aa24d5b2ab84e9c
Videsh Ravi <videshravi27@gmail.com>
2024-11-13 23:41:51 +0530

    api enhanced to assign issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import AuthOptions from "@/app/auth/AuthOptions";
-import { valid } from "@/app/valid";
+import { patchIssueSchema } from "@/app/valid";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -12,9 +12,20 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = valid.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const { assignedToUserId, title, description } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: assignedToUserId,
+      },
+    });
+    if (!user)
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
 
   const issue = await prisma.issue.findUnique({
@@ -29,8 +40,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
